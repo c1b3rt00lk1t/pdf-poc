2171449ab4ac793229bdbd04d4ce371c6cfed4ee
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-11-14 23:20:03 +0100

    General comments in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+/**
+ * This app will allow users to manipulated pdf files. The user will be able to do (at first) the following:
+ * - Combine pdf files
+ * - Split pdf files
+ * - Add page numbers to pdf files
+ * It will use the following libraries: pdf-lib, react-pdf, react-dropzone
+ * It will have light and dark mode
+ * It will have a simple UI, made with Chakra UI
+ * The home page has to display the three actions the user can do
+ * The App has to have a Layout component that will be used in all pages
+ *
+ */
+
 import "./App.css";
 
 function App() {
-  return <div>hello world"</div>;
+  return <div className="App">hello world</div>;
 }
 
 export default App;
