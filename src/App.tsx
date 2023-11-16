8abf6641373e3abe10b5c5392972d8ef921ae4ce
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-11-16 07:59:00 +0100

    Add header component to App

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,17 @@
  */
 
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main></main>
+    </>
+  );
 }
 
 export default App;
