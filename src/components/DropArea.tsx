f21c168779543ce1099a6be85811cba2183dc00a
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-11-16 15:54:17 +0100

    Input to be able to select a file

--- src/components/DropArea.tsx
+++ src/components/DropArea.tsx
@@ -1,5 +1,18 @@
-const DropArea = () => {
-  return <div>DropArea</div>;
+/** In a first mvp the component will have an input that allows to select on pdf file from one folder and stores its content to be able to modify it in a later stage*/
+
+import { ChangeEvent } from "react";
+
+export interface DropAreaProps {
+  handleChangeInput: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const DropArea = ({ handleChangeInput }: DropAreaProps) => {
+  return (
+    <div>
+      <h1>Drop Area</h1>
+      <input type="file" onChange={handleChangeInput} />
+    </div>
+  );
 };
 
 export default DropArea;
