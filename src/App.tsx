a608e4603a09f358495326cbe182cc547a422d3d
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-11-16 06:27:33 +0100

    Add material ui css baseline

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,21 @@
  * It will use the following libraries: pdf-lib, react-pdf, react-dropzone
  * It will have light and dark mode
  * The home page has to display the three actions the user can do
- * The App has to have a Layout component that will be used in all pages
- *
+ * The App must have a Layout, Header, Footer and Sidebar components that will be used in all pages
+ * The App must work offline and online
  */
 
 import "./App.css";
+import { Fragment } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <Fragment>
+      <CssBaseline />
+      <div className="App">hello world</div>
+    </Fragment>
+  );
 }
 
 export default App;
