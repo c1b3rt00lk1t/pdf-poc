e7271a273f178b37a9b1e0fc5798e6827683adfc
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-11-17 09:14:25 +0100

    Add a checkbox  toggle for multiple file selection

--- src/components/DropArea.tsx
+++ src/components/DropArea.tsx
@@ -1,9 +1,9 @@
 /** In a first mvp the component will have an input that allows to select on pdf file from one folder and stores its content to be able to modify it in a later stage
  * Only pdf files will be allowed
- * The input must be able to optionally accept multiple files.
+ * The input must be able to optionally accept multiple files. A checkbox will allow to select if the user wants to select one or multiple files
  */
 
-import { ChangeEvent, useRef } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import styles from "./DropArea.module.css";
 
 export interface DropAreaProps {
@@ -12,6 +12,8 @@ export interface DropAreaProps {
 }
 
 const DropArea = ({ handleChangeInput, handleClickReset }: DropAreaProps) => {
+  const [multipleFiles, setMultipleFiles] = useState<boolean>(false);
+
   const refInput = useRef<HTMLInputElement>(null);
   const resetInput = (ref: React.RefObject<HTMLInputElement>) => {
     if (ref.current) {
@@ -26,20 +28,27 @@ const DropArea = ({ handleChangeInput, handleClickReset }: DropAreaProps) => {
         <input
           type="file"
           accept=".pdf"
-          multiple={false}
+          multiple={multipleFiles}
           onChange={handleChangeInput}
           ref={refInput}
         />
       </div>
-      <button
-        className={styles.button}
-        onClick={() => {
-          handleClickReset();
-          resetInput(refInput);
-        }}
-      >
-        Reset
-      </button>
+      <div>
+        <button
+          className={styles.button}
+          onClick={() => {
+            handleClickReset();
+            resetInput(refInput);
+          }}
+        >
+          Reset
+        </button>
+        <input
+          type="checkbox"
+          onChange={() => setMultipleFiles((multipleFiles) => !multipleFiles)}
+        />{" "}
+        Allow multiple files
+      </div>
     </div>
   );
 };
