41f31810e0bfe8bc346b9e5b09664b9c39ef8ce9
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-11-18 22:17:16 +0100

    Extract addPageNumbers logic in a utils file

--- src/utils/pdf-utils.ts
+++ src/utils/pdf-utils.ts
@@ -1,4 +1,42 @@
-import { PDFDocument } from "pdf-lib";
+import { PDFDocument, StandardFonts, rgb } from "pdf-lib";
+
+// addPageNumbers will add page numbers to the pdf file
+// in the first mvp it will add a sequential number to each page, starting from 2 and excluding the first page
+// it will use the pdf-lib library
+// it will use a font type Arial and a font size of 12
+// it will add the page centered in the middle of the page and in the bottom of the page
+export async function addPageNumbers(file: File) {
+  const reader = new FileReader();
+  reader.readAsArrayBuffer(file);
+  reader.onloadend = async () => {
+    const pdfData = reader.result as ArrayBuffer;
+    const pdfDoc = await PDFDocument.load(pdfData);
+    const pages = pdfDoc.getPages();
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+    pages.forEach((page, index) => {
+      if (index !== 0) {
+        page.drawText((index + 1).toString(), {
+          x: page.getWidth() / 2,
+          y: 10,
+          size: 9,
+          font: helveticaFont,
+          color: rgb(0, 0, 0),
+        });
+      }
+    });
+
+    const pdfBytes = await pdfDoc.save();
+    const blob = new Blob([pdfBytes], { type: "application/pdf" });
+    const url = URL.createObjectURL(blob);
+    const downloadLink = document.createElement("a");
+    const fileName = `${file.name.slice(0, -4)} - pages.pdf`;
+
+    downloadLink.href = url;
+    downloadLink.download = fileName;
+    downloadLink.click();
+  };
+}
 
 /**
  * splitFiles splits the pdf file using the input that the user provides
