a11c0c354acc004eb679f04c1cc01e9c52aba6bb
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-11-18 21:59:13 +0100

    Add Transform combine component

--- src/components/Transformations.tsx
+++ src/components/Transformations.tsx
@@ -5,9 +5,9 @@
  * It will provide a button to download the modified pdf file
  */
 
-import { PDFDocument } from "pdf-lib";
 import TransformSplit from "./TransformSplit";
 import TransformPages from "./TransformPages";
+import TransformCombine from "./TransformCombine";
 
 interface TransformationsProps {
   action: "combine" | "split" | "pages";
@@ -15,38 +15,6 @@ interface TransformationsProps {
 }
 
 const Transformations = ({ files, action }: TransformationsProps) => {
-  // combineFiles will combine the pdf files that the user has selected
-  // it will use the pdf-lib library
-  // the output file will be a file that contains all the pages of the input files
-
-  async function combineFiles() {
-    const outputDoc = await PDFDocument.create();
-
-    const orderedFiles = [...files].sort((a, b) => (a.name > b.name ? 1 : -1));
-
-    for (const file of orderedFiles) {
-      const fileArrayBuffer = await file.arrayBuffer();
-      const inputDoc = await PDFDocument.load(fileArrayBuffer);
-      const pages = await outputDoc.copyPages(
-        inputDoc,
-        inputDoc.getPageIndices()
-      );
-      for (const page of pages) {
-        outputDoc.addPage(page);
-      }
-    }
-
-    const pdfBytes = await outputDoc.save();
-    const blob = new Blob([pdfBytes], { type: "application/pdf" });
-    const url = URL.createObjectURL(blob);
-    const downloadLink = document.createElement("a");
-    const fileName = "combined.pdf";
-
-    downloadLink.href = url;
-    downloadLink.download = fileName;
-    downloadLink.click();
-  }
-
   const actionTitles = {
     combine: "Combine files",
     split: "Split files",
@@ -60,10 +28,7 @@ const Transformations = ({ files, action }: TransformationsProps) => {
         <TransformPages file={files[0]} />
       )}
       {action === "combine" && files.length > 0 && (
-        <>
-          <p>{files.length} files selected</p>
-          <button onClick={combineFiles}>Combine files</button>
-        </>
+        <TransformCombine files={files} />
       )}
       {action === "split" && files.length > 0 && (
         <TransformSplit file={files[0]} />
