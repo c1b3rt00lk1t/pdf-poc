ba714ae0b111662b8a00842e4074e91107d2dc0a
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-11-18 23:51:46 +0100

    Basic drop area that accepts only pdfs

--- src/components/DropArea.tsx
+++ src/components/DropArea.tsx
@@ -1,7 +1,47 @@
 import styles from "./DropArea.module.css";
+import { useState } from "react";
 
 const DropArea = () => {
-  return <div className={styles.DropArea}>DropArea</div>;
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleDragOver = (event: React.DragEvent) => {
+    event.stopPropagation();
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "copy";
+  };
+
+  const handleDrop = (event: React.DragEvent) => {
+    event.preventDefault();
+    console.log(event.dataTransfer.files[0]);
+    if (event.dataTransfer && event.dataTransfer.items) {
+      if (event.dataTransfer.files[0].type === "application/pdf") {
+        setFile(event.dataTransfer.files[0]);
+      } else {
+        alert("Please drop a pdf file");
+      }
+    }
+  };
+
+  const handleClick = () => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = ".pdf";
+    input.onchange = (event) => {
+      setFile((event.target as HTMLInputElement).files![0]);
+    };
+    input.click();
+  };
+
+  return (
+    <div
+      className={styles.DropArea}
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+      onClick={handleClick}
+    >
+      {file ? file.name : "Drop file here or click to select"}
+    </div>
+  );
 };
 
 export default DropArea;
