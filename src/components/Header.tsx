2c40585ee3afa2719e59c4d3f9176c1a8b2dc6d4
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-11-20 19:22:19 +0100

    Header padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
+import styles from "./Header.module.css";
+
 const Header = () => {
-  return <h2>PDF Utils</h2>;
+  return <h2 className={styles.Header}>PDF Utils</h2>;
 };
 
 export default Header;
