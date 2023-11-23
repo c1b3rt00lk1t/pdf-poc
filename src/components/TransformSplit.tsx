2e28f73422cf9d4d894df1338da5973c5656fc78
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-11-23 14:32:18 +0100

    Input basename and pass to splitFiles

--- src/components/TransformSplit.tsx
+++ src/components/TransformSplit.tsx
@@ -1,3 +1,11 @@
+/**
+ * The TransformSplit component is responsible for managing the settings for the split operation.
+ * The inputs and buttons are disabled when there is no file loaded.
+ * When the button is clicked, the splitFiles function is called with the page ranges, the file and a base name.
+ * One input allows to define a base name for the output files.
+ * One input allows to define a series of pages or page ranges using ',' and '-'.
+ */
+
 import { useState } from "react";
 import { splitFiles } from "../utils/pdf-utils";
 
@@ -7,21 +15,29 @@ interface TransformSplitProps {
 
 const TransformSplit = ({ file }: TransformSplitProps) => {
   const [pageRanges, setPageRanges] = useState<string>("");
-
+  const [basename, setBasename] = useState<string>("");
+  const disabled = !file;
   return (
     <>
+      <input
+        onChange={(ev) => setBasename(ev.target.value)}
+        type="text"
+        placeholder={!disabled ? file.name.replace(/.pdf/i, "") : "Basename"}
+        value={!disabled ? basename : ""}
+        disabled={disabled}
+      />
       <input
         onChange={(ev) => {
           setPageRanges(ev.target.value);
         }}
         type="text"
         placeholder="1,2,3-5"
-        value={!!file ? pageRanges : ""}
-        disabled={!file}
+        value={!disabled ? pageRanges : ""}
+        disabled={disabled}
       />
       <button
-        disabled={!file}
-        onClick={() => file && splitFiles(pageRanges, file)}
+        disabled={disabled}
+        onClick={() => file && splitFiles(pageRanges, file, basename)}
       >
         Split files
       </button>
