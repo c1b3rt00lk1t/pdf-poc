ebe6d8e01252b305eddec4c9ed67549d9da81d0b
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-11-28 08:29:04 +0100

    Custom hook to detect the device type (mobile)

---
+++ src/hooks/useDeviceType.tsx
@@ -0,0 +1,37 @@
+/**
+ * Custom hook to detect device type
+ * based on https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent
+ */
+
+import { useState, useEffect } from "react";
+
+export function useDeviceType() {
+  const [deviceType, setDeviceType] = useState<string>("");
+
+  useEffect(() => {
+    let hasTouchScreen = false;
+    if ("maxTouchPoints" in navigator) {
+      hasTouchScreen = navigator.maxTouchPoints > 0;
+    } else {
+      const mQ = matchMedia("(pointer:coarse)");
+      if (mQ && mQ.media === "(pointer:coarse)") {
+        hasTouchScreen = !!mQ.matches;
+      } else if ("orientation" in window) {
+        hasTouchScreen = true; // deprecated, but good fallback
+      } else {
+        // Only as a last resort, fall back to user agent sniffing
+        var UA = (navigator as Navigator).userAgent;
+        hasTouchScreen =
+          /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+          /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+      }
+    }
+    if (hasTouchScreen) {
+      setDeviceType("Mobile");
+    } else {
+      setDeviceType("Desktop");
+    }
+  }, []);
+
+  return deviceType;
+}
