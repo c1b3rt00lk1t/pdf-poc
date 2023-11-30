538621e5e6f544ddefcafad1a767ed8d9bce1367
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-11-30 07:05:57 +0100

    Set initial state for match media hook

--- src/hooks/useMatchMedia.tsx
+++ src/hooks/useMatchMedia.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 export function useMatchMedia(query: string) {
-  const [matches, setMatches] = useState(false);
+  const [matches, setMatches] = useState(window.matchMedia(query).matches);
   useEffect(() => {
     const matchQueryList = window.matchMedia(query);
     function handleChange(event: MediaQueryListEvent) {
