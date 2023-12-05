7356adb4bb7914967b8d14d70baf979512cb8118
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-05 16:39:19 +0100

    It shows the buttons names in large screen

--- cypress/e2e/pdf-poc.cy.ts
+++ cypress/e2e/pdf-poc.cy.ts
@@ -1,7 +1,21 @@
 /// <reference types="cypress" />
 
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("http://127.0.0.1:5173/");
+describe("Test pdf-poc spec", () => {
+  beforeEach(() => {
+    cy.viewport(1600, 900);
+    cy.visit("localhost:5173/");
+  });
+
+  it("shows the buttons names in large screen", () => {
+    cy.get("h2").contains("PDF Utils").should("be.visible");
+    cy.get("span").contains("Remove").should("be.visible");
+    cy.get("span").contains("Rotate").should("be.visible");
+    cy.get("span").contains("Combine").should("be.visible");
+    cy.get("span").contains("Numbers").should("be.visible");
+    cy.get("span").contains("Watermark").should("be.visible");
+    cy.get("span").contains("Split").should("be.visible");
+    cy.get("button").contains("Add").should("be.visible");
+    cy.get("button").contains("Reset").should("be.visible");
+    cy.get("button").contains("Order").should("be.visible");
   });
 });
