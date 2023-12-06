578d059efd443b73daddbd884325ca02563dc6fd
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-06 17:52:46 +0100

    Test it has the combine action by defaul

--- cypress/e2e/combine-action.cy.ts
+++ cypress/e2e/combine-action.cy.ts
@@ -5,4 +5,19 @@ describe("Test combine action page", () => {
     cy.visit("localhost:5173/");
     cy.viewport(375, 667);
   });
+
+  it("has the combine action by default", () => {
+    // checks a dynamic class that contains the word "selected"
+    cy.get("div")
+      .contains("Combine")
+      .parent()
+      .invoke("attr", "class")
+      .should("contain", "selected");
+
+    // checks the specific background color
+    cy.get("div")
+      .contains("Combine")
+      .parent()
+      .should("have.css", "background-color", "rgba(84, 172, 128, 0.2)");
+  });
 });
