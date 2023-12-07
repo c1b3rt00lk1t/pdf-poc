3ece8bd850d4e323c7f32e63b9e5c136935e1b7e
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-07 22:54:43 +0100

    Test it renders

---
+++ src/components/FileList.test.tsx
@@ -0,0 +1,20 @@
+//Test for FileList component
+
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import FileList from "./FileList";
+import { FileListProps } from "./FileList";
+
+describe("Test FileList component", () => {
+  const props: FileListProps = {
+    files: [],
+    setFiles: jest.fn(),
+    orderFiles: [],
+    setOrderFiles: jest.fn(),
+    showList: true,
+    isMobile: false,
+  };
+  test("renders FileList component", () => {
+    render(<FileList {...props} />);
+  });
+});
