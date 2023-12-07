6160b68648092f8bdc0773f8471bfb560b616c01
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-07 23:41:02 +0100

    Test TransformCombine renders

---
+++ src/components/TransformCombine.test.tsx
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import TransformCombine from "./TransformCombine";
+import { TransformCombineProps } from "./TransformCombine";
+
+describe("Test TransformCombine", () => {
+  test("it renders", () => {
+    const props: TransformCombineProps = {
+      files: [new File(["hello"], "hello.pdf", new File(["bye"], "bye.pdf"))],
+      orderFiles: [1, 0],
+      handleKeepOutputAsInput: jest.fn(),
+      basename: "basename",
+      setBasename: jest.fn(),
+      isMobile: true,
+    };
+    render(<TransformCombine {...props} />);
+  });
+});
