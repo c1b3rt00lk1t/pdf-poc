a5e060ef45fed03ef8ecbd045184d90adf79f8e9
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-22 19:06:41 +0100

    Test changes options and reset (wrapper)

--- src/components/TransformSplit.test.tsx
+++ src/components/TransformSplit.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
+import { useState } from "react";
 
 import TransformSplit, { TransformSplitProps } from "./TransformSplit";
 
@@ -45,5 +46,43 @@ describe("Test TransformSplit component", () => {
 
     // Assertion
     expect(checkbox).not.toBeChecked();
+  });
+
+  test("changes options and reset (wrapper)", async () => {
+    // Render the component
+    function TestWrapper() {
+      const [basename, setBasename] = useState("basename");
+      const props = {
+        ...defaultProps,
+        basename: basename,
+        setBasename: setBasename,
+      };
+      return <TransformSplit {...props} />;
+    }
+    render(<TestWrapper />);
+
+    // Interact with the input text
+    const basenameInput = screen.getByRole("textbox", { name: /Base name/i });
+    const pageRangesInput = screen.getByRole("textbox", {
+      name: /Page ranges/i,
+    });
+
+    // Change the input text
+    await userEvent.type(basenameInput, "test");
+    await userEvent.type(pageRangesInput, "1,2,3-5");
+
+    // Assertions
+    expect(basenameInput).toHaveValue("basenametest");
+    expect(pageRangesInput).toHaveValue("1,2,3-5");
+
+    // Interact with the buttons
+    const resetButton = screen.getByRole("button", { name: /Reset/i });
+
+    // Click on the buttons
+    await userEvent.click(resetButton);
+
+    // Assertions
+    expect(basenameInput).toHaveValue("");
+    expect(pageRangesInput).toHaveValue("");
   });
 });
