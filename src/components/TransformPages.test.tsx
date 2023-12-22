1d3a0cd93afbd5f122d848ae13a7ab6f1aecd11c
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-22 17:19:07 +0100

    Enhance it renders test

--- src/components/TransformPages.test.tsx
+++ src/components/TransformPages.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import TransformPages from "./TransformPages";
@@ -12,6 +12,18 @@ const defaultrops: TransformPagesProps = {
 
 describe("Test TransformPages component", () => {
   test("it renders", () => {
+    // Render the component
     render(<TransformPages {...defaultrops} />);
+
+    // Assertions
+    expect(screen.getByText("Add numbers")).toBeInTheDocument();
+    expect(screen.getByText("Reset")).toBeInTheDocument();
+    expect(screen.getByText("Keep output as next input")).toBeInTheDocument();
+    expect(screen.getByLabelText("Font size")).toBeInTheDocument();
+    expect(screen.getByLabelText("Font")).toBeInTheDocument();
+    expect(screen.getByText("Horizontal alignment")).toBeInTheDocument();
+    expect(screen.getByText("Bottom margin (cm)")).toBeInTheDocument();
+    expect(screen.getByText("Initial page")).toBeInTheDocument();
+    expect(screen.getByText("Start number")).toBeInTheDocument();
   });
 });
