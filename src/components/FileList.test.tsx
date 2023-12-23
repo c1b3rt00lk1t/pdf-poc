99832f9e4634c7f937f0e2be5b6464d3997dc5c4
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-23 13:45:41 +0100

    Extract defaultProps

--- src/components/FileList.test.tsx
+++ src/components/FileList.test.tsx
@@ -3,16 +3,17 @@ import "@testing-library/jest-dom";
 import FileList from "./FileList";
 import { FileListProps } from "./FileList";
 
+const defaultProps: FileListProps = {
+  files: [],
+  setFiles: jest.fn(),
+  orderFiles: [],
+  setOrderFiles: jest.fn(),
+  showList: true,
+  isMobile: false,
+};
+
 describe("Test FileList component", () => {
-  const props: FileListProps = {
-    files: [],
-    setFiles: jest.fn(),
-    orderFiles: [],
-    setOrderFiles: jest.fn(),
-    showList: true,
-    isMobile: false,
-  };
   test("renders FileList component", () => {
-    render(<FileList {...props} />);
+    render(<FileList {...defaultProps} />);
   });
 });
