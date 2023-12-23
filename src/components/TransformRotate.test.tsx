24366a86f981a6a51ee3eeeba4af3965131e0e2d
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-23 12:45:40 +0100

    Extract defaultProps

--- src/components/TransformRotate.test.tsx
+++ src/components/TransformRotate.test.tsx
@@ -4,13 +4,14 @@ import "@testing-library/jest-dom";
 import TransformRotate from "./TransformRotate";
 import { TransformRotateProps } from "./TransformRotate";
 
+const defaultProps: TransformRotateProps = {
+  file: new File(["test"], "test.pdf"),
+  handleKeepOutputAsInput: jest.fn(),
+  isMobile: true,
+};
+
 describe("Test TransformRotate component", () => {
   test("ir renders", () => {
-    const props: TransformRotateProps = {
-      file: new File(["test"], "test.pdf"),
-      handleKeepOutputAsInput: jest.fn(),
-      isMobile: true,
-    };
-    render(<TransformRotate {...props} />);
+    render(<TransformRotate {...defaultProps} />);
   });
 });
