fd307d5653d5065773fd385bbd77874b24ba0c51
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-23 14:16:34 +0100

    Extract default props

--- src/components/FileSelection.test.tsx
+++ src/components/FileSelection.test.tsx
@@ -4,18 +4,19 @@ import "@testing-library/jest-dom";
 import { FileSelectionProps } from "./FileSelection";
 import FileSelection from "./FileSelection";
 
+const defaultProps: FileSelectionProps = {
+  files: [],
+  setFiles: jest.fn(),
+  orderFiles: [],
+  setOrderFiles: jest.fn(),
+  handleClickReset: jest.fn(),
+  action: "combine",
+  isMobile: false,
+  setBasename: jest.fn(),
+};
+
 describe("Test FileSelection component", () => {
-  const props: FileSelectionProps = {
-    files: [],
-    setFiles: jest.fn(),
-    orderFiles: [],
-    setOrderFiles: jest.fn(),
-    handleClickReset: jest.fn(),
-    action: "combine",
-    isMobile: false,
-    setBasename: jest.fn(),
-  };
   test("renders FileSelection component", () => {
-    render(<FileSelection {...props} />);
+    render(<FileSelection {...defaultProps} />);
   });
 });
