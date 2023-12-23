cefe3fe89b829b605a430115de45142c1e751100
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-23 14:17:27 +0100

    Test renders FileSelection component with files

--- src/components/FileSelection.test.tsx
+++ src/components/FileSelection.test.tsx
@@ -15,8 +15,19 @@ const defaultProps: FileSelectionProps = {
   setBasename: jest.fn(),
 };
 
+const files = [
+  new File(["test"], "test1.pdf"),
+  new File(["test"], "test2.pdf"),
+  new File(["test"], "test3.pdf"),
+];
+
 describe("Test FileSelection component", () => {
   test("renders FileSelection component", () => {
+    // Render the component
     render(<FileSelection {...defaultProps} />);
+  });
+  test("renders FileSelection component with files", () => {
+    // Render the component
+    render(<FileSelection {...defaultProps} files={files} />);
   });
 });
