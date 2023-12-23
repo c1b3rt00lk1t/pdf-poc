efe71467950e399e63e72bfa35feba9f86e58fc3
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-23 19:49:45 +0100

    createPdfFile helper function

--- src/utils/pdf-utils.test.ts
+++ src/utils/pdf-utils.test.ts
@@ -1,18 +1,23 @@
 import { combineFiles } from "./pdf-utils.ts";
 
 describe("combineFiles", () => {
+  // Helper function to create a PDF file from content
+  const pdfContent = "file content";
+  const createPdfFile = (name: string): File => {
+    const fileContent = [pdfContent];
+    return new File(fileContent, name, {
+      type: "application/pdf",
+    });
+  };
+
   test("should combine files in the correct order", async () => {
     // Arrange
     const pdfContent = "file1 content"; // Replace this with your actual PDF content
 
     // Create a File with content as an array of strings
     const fileContent = [pdfContent];
-    const file1 = new File(fileContent, "file1.pdf", {
-      type: "application/pdf",
-    });
-    const file2 = new File(fileContent, "file2.pdf", {
-      type: "application/pdf",
-    });
+    const file1 = createPdfFile("file1.pdf");
+    const file2 = createPdfFile("file2.pdf");
 
     const files = [file1, file2];
     const order = [1, 0]; // Swap the order of the files
