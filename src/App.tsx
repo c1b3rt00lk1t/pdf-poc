d40b48125e898f0a1c5e52f229e4d99bc6a9c264
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2024-01-27 22:01:00 +0100

    Remove obsolete comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,3 @@
-/**
- * This app will allow users to manipulated pdf files. The user will be able to do (at first) the following:
- * - Combine pdf files
- * - Split pdf files
- * - Add page numbers to pdf files
- * It will use the following libraries: pdf-lib
- * It will have light and dark mode
- * The home page has to display the three actions the user can do
- * The App must have a Layout, Header, Footer and Sidebar components that will be used in all pages
- * The App must work offline and online
- */
-
 import { useState } from "react";
 import "./index.css";
 import FileSelection from "./components/FileSelection";
