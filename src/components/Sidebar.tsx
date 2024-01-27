d770fd512000792ac082b6e6600a9f55c8fe17d2
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2024-01-27 23:30:49 +0100

    Tooltips in Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,6 +8,7 @@ import {
   MdOutlineBrandingWatermark,
   MdOutlineCallSplit,
 } from "react-icons/md";
+import { Tooltip } from "antd";
 
 export interface SidebarProps {
   handleClickAction: (action: Action) => void;
@@ -18,73 +19,95 @@ export interface SidebarProps {
 const Sidebar = ({ handleClickAction, action, isMobile }: SidebarProps) => {
   return (
     <div className={isMobile ? styles.SidebarMobile : styles.Sidebar}>
-      <div
-        onClick={() => {}}
-        className={`${isMobile ? styles.buttonMobile : styles.button} ${
-          styles.disabled
-        }`}
+      <Tooltip
+        placement="top"
+        title={"Coming soon"}
+        arrow={true}
+        color={"grey"}
       >
-        <MdOutlineContentCut />
-        <span className={isMobile ? styles.spanMobile : styles.span}>
-          Remove
-        </span>
-      </div>
-      <div
-        onClick={() => handleClickAction("rotate")}
-        className={`${action === "rotate" ? styles.selected : undefined} ${
-          isMobile ? styles.buttonMobile : styles.button
-        }`}
+        <div
+          onClick={() => {}}
+          className={`${isMobile ? styles.buttonMobile : styles.button} ${
+            styles.disabled
+          }`}
+        >
+          <MdOutlineContentCut />
+          <span className={isMobile ? styles.spanMobile : styles.span}>
+            Remove
+          </span>
+        </div>
+      </Tooltip>
+      <Tooltip placement="top" title={"Rotate page(s)"} arrow={true}>
+        <div
+          onClick={() => handleClickAction("rotate")}
+          className={`${action === "rotate" ? styles.selected : undefined} ${
+            isMobile ? styles.buttonMobile : styles.button
+          }`}
+        >
+          <MdOutlineRotateRight />
+          <span className={isMobile ? styles.spanMobile : styles.span}>
+            Rotate
+          </span>
+        </div>
+      </Tooltip>
+      <Tooltip placement="top" title={"Combine files"} arrow={true}>
+        <div
+          className={`${action === "combine" ? styles.selected : undefined} ${
+            isMobile ? styles.buttonMobile : styles.button
+          }`}
+          onClick={() => handleClickAction("combine")}
+        >
+          <MdOutlineCallMerge />
+          <span className={isMobile ? styles.spanMobile : styles.span}>
+            Combine
+          </span>
+        </div>
+      </Tooltip>
+      <Tooltip placement="top" title={"Page numbers"} arrow={true}>
+        <div
+          className={`${action === "pages" ? styles.selected : undefined} ${
+            isMobile ? styles.buttonMobile : styles.button
+          }`}
+          onClick={() => handleClickAction("pages")}
+        >
+          <MdOutlineFormatListNumbered />
+          <span className={isMobile ? styles.spanMobile : styles.span}>
+            Numbers
+          </span>
+        </div>
+      </Tooltip>
+      <Tooltip
+        placement="top"
+        title={"Coming soon"}
+        arrow={true}
+        color={"grey"}
       >
-        <MdOutlineRotateRight />
-        <span className={isMobile ? styles.spanMobile : styles.span}>
-          Rotate
-        </span>
-      </div>
-      <div
-        className={`${action === "combine" ? styles.selected : undefined} ${
-          isMobile ? styles.buttonMobile : styles.button
-        }`}
-        onClick={() => handleClickAction("combine")}
-      >
-        <MdOutlineCallMerge />
-        <span className={isMobile ? styles.spanMobile : styles.span}>
-          Combine
-        </span>
-      </div>
-      <div
-        className={`${action === "pages" ? styles.selected : undefined} ${
-          isMobile ? styles.buttonMobile : styles.button
-        }`}
-        onClick={() => handleClickAction("pages")}
-      >
-        <MdOutlineFormatListNumbered />
-        <span className={isMobile ? styles.spanMobile : styles.span}>
-          Numbers
-        </span>
-      </div>
-      <div
-        onClick={() => {}}
-        className={`${isMobile ? styles.buttonMobile : styles.button} ${
-          styles.disabled
-        }`}
-      >
-        <MdOutlineBrandingWatermark />
-        <span className={isMobile ? styles.spanMobile : styles.span}>
-          Watermark
-        </span>
-      </div>
+        <div
+          onClick={() => {}}
+          className={`${isMobile ? styles.buttonMobile : styles.button} ${
+            styles.disabled
+          }`}
+        >
+          <MdOutlineBrandingWatermark />
+          <span className={isMobile ? styles.spanMobile : styles.span}>
+            Watermark
+          </span>
+        </div>
+      </Tooltip>
 
-      <div
-        className={`${action === "split" ? styles.selected : undefined} ${
-          isMobile ? styles.buttonMobile : styles.button
-        }`}
-        onClick={() => handleClickAction("split")}
-      >
-        <MdOutlineCallSplit />
-        <span className={isMobile ? styles.spanMobile : styles.span}>
-          Split
-        </span>
-      </div>
+      <Tooltip placement="top" title={"Split file"} arrow={true}>
+        <div
+          className={`${action === "split" ? styles.selected : undefined} ${
+            isMobile ? styles.buttonMobile : styles.button
+          }`}
+          onClick={() => handleClickAction("split")}
+        >
+          <MdOutlineCallSplit />
+          <span className={isMobile ? styles.spanMobile : styles.span}>
+            Split
+          </span>
+        </div>
+      </Tooltip>
     </div>
   );
 };
